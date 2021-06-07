662cfded824171a2b8b606f371b1a22f5ec14bde
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-06-07 15:34:17 +0200

    palindroma

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri. Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto. */
 
-var min = 1;
+/* var min = 1;
 var max = 5;
 
 do var parDisp = prompt("Pari o dispari?").toLowerCase();
@@ -26,5 +26,26 @@ function numRandom(min, max){
 function pariDispari(sum){
     if (sum % 2 == 0) return "pari";
     else return "dispari";
-}
+} */
 
+/* Palidroma: Chiedere all’utente di inserire una parola. Creare una funzione per capire se la parola inserita è palindroma */
+
+var parola = prompt("Inserisci una parola, ti dirò se è palindroma");
+var esito = " non è palindroma";
+if (isPal(parola)) esito = " è palindroma"
+document.getElementById("text").innerHTML = "La parola \"" + parola + "\"" + esito;
+
+function isPal(parola) {
+    var i = 0;
+    var j = parola.length - 1;
+
+    while (i < parola.length / 2 && j > parola.length / 2) {
+        if (parola.charAt(i) != parola.charAt(j)) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+
+    return true;
+}
