5a421d09ce4b0cad2d6259be793ae766d4cc2c03
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-06-07 15:19:21 +0200

    fine pari o dispari

--- js/script.js
+++ js/script.js
@@ -2,12 +2,29 @@
 
 var min = 1;
 var max = 5;
-/* do var numUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
-while (numUtente > max || numUtente < min); */
+
+do var parDisp = prompt("Pari o dispari?").toLowerCase();
+while (parDisp != "pari" && parDisp != "dispari");
+
+do var numUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+while (numUtente > max || numUtente < min || Number.isNaN(numUtente));
 
 var numRand = numRandom(min, max + 1);
+var sum = numUtente + numRand;
+var esito = "hai perso!";
+
+if (sum == parDisp ){
+    esito = "hai vinto!"
+}
+
+document.getElementById("text").innerHTML = "Scelta: " + parDisp + "<br>Numero utente: " + numUtente + "<br> Numero random: " + numRand + "<br>La somma è " + pariDispari(sum) + ": " + esito;
 
 function numRandom(min, max){
     return Math.floor(Math.random() * (max - min) + min);
+}
+
+function pariDispari(sum){
+    if (sum % 2 == 0) return "pari";
+    else return "dispari";
 }
 
